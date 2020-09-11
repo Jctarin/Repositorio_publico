6c3bb56b3818b7b0aa07aea0c7fd35167b595570
Juatarto <juatarto@gmail.com>
2020-09-11 22:46:01 +0200

    Version Autopiloto

--- Autopiloto_Def/GUI_main.py
+++ Autopiloto_Def/GUI_main.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 
 raiz = Tk()
-raiz.title("Autopilot")
+raiz.title("Autopilot v1.1")
 raiz.iconbitmap("Airplane_2.ico")
 raiz.resizable(False, False)
 L.Pagina_principal(raiz)
